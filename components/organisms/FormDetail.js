6347f6ac00fb1ad6c73666d2b318d28e1c05a077
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-08-06 21:41:07 +0700

    [update] page detail

--- components/organisms/FormDetail.js
+++ components/organisms/FormDetail.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
-import Tabs from '@mui/material/Tabs';
+import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 import style from './styles/detail.module.css';
 
@@ -10,7 +13,7 @@ import { IoBookmarkOutline } from 'react-icons/io5';
 import { FiPlay } from 'react-icons/fi';
 
 const FormDetail = ({ data }) => {
-  const [value, setValue] = React.useState('one');
+  const [value, setValue] = React.useState('1');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -22,120 +25,66 @@ const FormDetail = ({ data }) => {
         <div className={style.iconBack}>
           <BiArrowBack size={30} color="#F5F5F5" />
         </div>
-        {!data.length ? (
+        {/* {!data.length ? (
           <></>
         ) : (
-          data.map((item, index) => (
-            <div className={style.img}>
-              <div className="row w-100">
-                <div className="col-8">
-                  {/* <Image src="/img/picture1.png" height={500} width={700} /> */}
-                  <h3>{item.name_recipe}</h3>
-                  <p>By Chef Ronald Humson</p>
-                </div>
-                <div className="col-4 p-0">
-                  <div className={style.buttonControl}>
-                    <button type="button" className={style.save}>
-                      <IoBookmarkOutline size={20} />
-                    </button>
-                    <button type="button" className={style.like}>
-                      <BiLike size={20} />
-                    </button>
-                  </div>
-                </div>
+          data.map((item, index) => ( */}
+        <div className={style.img}>
+          <div className="row w-100">
+            <div className="col-8">
+              {/* <Image src="/img/picture1.png" height={500} width={700} /> */}
+              <h3>bakso</h3>
+              <p>By Chef Ronald Humson</p>
+            </div>
+            <div className="col-4 p-0">
+              <div className={style.buttonControl}>
+                <button type="button" className={style.save}>
+                  <IoBookmarkOutline size={20} />
+                </button>
+                <button type="button" className={style.like}>
+                  <BiLike size={20} />
+                </button>
               </div>
             </div>
-          ))
-        )}
+          </div>
+        </div>
+        {/* ))
+        )} */}
 
         <div className={style.content}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            textColor="secondary"
-            indicatorColor="secondary"
-            aria-label="secondary tabs example"
-          >
-            <Tab value="one" label="Ingredients" />
-            {/* - 2 slices whole-grain bread (bakery-fresh recommended) - 1
-              tablespoon hummus - 2 slices tomato - 1/2 small cucumber, thinly
-              sliced lengthwise - 1 slice low-fat cheese
-            </Tab> */}
-            <Tab value="two" label="video-step" />
-            {/* <Tab
-              className={style.tab}
-              eventKey="ingredients"
-              title="Ingredients"
-            > */}
-
-            <Tab value="video-step" title="Video Step">
-              <div className={style.playContainer}>
-                <div className="row">
-                  <div className="col-4">
-                    <div className={style.buttonPlay}>
-                      <FiPlay size={30} color="#ffffff" />
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <TabList
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                >
+                  <Tab label="Ingredients" value="1" />
+                  <Tab label="Video Step" value="2" />
+                </TabList>
+              </Box>
+              <TabPanel value="1" label="Ingredients">
+                - 2 slices whole-grain bread (bakery-fresh recommended) - 1
+                tablespoon hummus - 2 slices tomato - 1/2 small cucumber, thinly
+                sliced lengthwise - 1 slice low-fat cheese
+              </TabPanel>
+              <TabPanel value="2" label="Video Step">
+                <div className={style.playContainer}>
+                  <div className="row">
+                    <div className="col-4">
+                      <div className={style.buttonPlay}>
+                        <FiPlay size={30} color="#ffffff" />
+                      </div>
+                    </div>
+                    <div className="col-8">
+                      <h4>Step 1</h4>
+                      <p>Boil eggs for 3 minutes</p>
                     </div>
                   </div>
-                  <div className="col-8">
-                    <h4>Step 1</h4>
-                    <p>Boil eggs for 3 minutes</p>
-                  </div>
                 </div>
-              </div>
-              <div className={style.playContainer}>
-                <div className="row">
-                  <div className="col-4">
-                    <div className={style.buttonPlay}>
-                      <FiPlay size={30} color="#ffffff" />
-                    </div>
-                  </div>
-                  <div className="col-8">
-                    <h4>Step 1</h4>
-                    <p>Boil eggs for 3 minutes</p>
-                  </div>
-                </div>
-              </div>
-              <div className={style.playContainer}>
-                <div className="row">
-                  <div className="col-4">
-                    <div className={style.buttonPlay}>
-                      <FiPlay size={30} color="#ffffff" />
-                    </div>
-                  </div>
-                  <div className="col-8">
-                    <h4>Step 1</h4>
-                    <p>Boil eggs for 3 minutes</p>
-                  </div>
-                </div>
-              </div>
-              <div className={style.playContainer}>
-                <div className="row">
-                  <div className="col-4">
-                    <div className={style.buttonPlay}>
-                      <FiPlay size={30} color="#ffffff" />
-                    </div>
-                  </div>
-                  <div className="col-8">
-                    <h4>Step 1</h4>
-                    <p>Boil eggs for 3 minutes</p>
-                  </div>
-                </div>
-              </div>
-              <div className={style.playContainer}>
-                <div className="row">
-                  <div className="col-4">
-                    <div className={style.buttonPlay}>
-                      <FiPlay size={30} color="#ffffff" />
-                    </div>
-                  </div>
-                  <div className="col-8">
-                    <h4>Step 1</h4>
-                    <p>Boil eggs for 3 minutes</p>
-                  </div>
-                </div>
-              </div>
-            </Tab>
-          </Tabs>
+              </TabPanel>
+            </TabContext>
+          </Box>
         </div>
       </div>
     </div>
