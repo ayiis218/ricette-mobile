b46351a217eb2b0172ab2cfdc7897b3c603332e4
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-08-24 22:46:20 +0700

    [update]

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,7 @@ const NotFound = () => {
             <div className="notfound-404">
                <h1>404</h1>
             </div>
-            <h2>Oops, Page NotFound!</h2>
+            <h2>in development process</h2>
             <span className="arrow" />
             <Link href="/">Return To Homepage</Link>
          </div>
