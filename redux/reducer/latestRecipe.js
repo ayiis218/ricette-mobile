8ec64f60a9c02aa396ae15654a56681a0d9a903d
ayiis <76609039+ayiis218@users.noreply.github.com>
2022-09-07 19:50:56 +0700

    [update]

--- redux/reducer/latestRecipe.js
+++ redux/reducer/latestRecipe.js
@@ -1,7 +1,7 @@
 import {
-   LATEST_RECIPE_PENDING,
-   LATEST_RECIPE_SUCCESS,
-   LATEST_RECIPE_FAILED,
+   GET_LATEST_RECIPE_PENDING,
+   GET_LATEST_RECIPE_SUCCESS,
+   GET_LATEST_RECIPE_FAILED,
 } from '../types';
 
 const initialState = {
@@ -13,16 +13,16 @@ const initialState = {
 
 const latestRecipe = (state = initialState, action) => {
    switch (action.type) {
-      case LATEST_RECIPE_PENDING:
+      case GET_LATEST_RECIPE_PENDING:
          return { ...state, isLoading: true };
-      case LATEST_RECIPE_SUCCESS:
+      case GET_LATEST_RECIPE_SUCCESS:
          return {
             ...state,
             isLoading: false,
             isError: false,
             data: action.payload.data,
          };
-      case LATEST_RECIPE_FAILED:
+      case GET_LATEST_RECIPE_FAILED:
          return {
             ...state,
             isLoading: false,
